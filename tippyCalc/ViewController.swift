ade509e5f8b805f38990d6274052852463054f50
Jameel Brice <jameelbrice@gmail.com>
2020-01-17 17:03:03 -0500

    Finished Tipper

--- tippyCalc/ViewController.swift
+++ tippyCalc/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-}
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+   
+    @IBAction func calculateTip(_ sender: Any) {
+        //GET BILL AMOUNT
+        let bill = Double(billField.text!) ?? 0
+             
+             //CALCULATE THE TIP AND TOTAL
+        let tipPercentages = [0.1,0.15,0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+             let total = bill + tip
+             //UPDATE THE TIP AND TOATAL
+             tipLabel.text = String(format: "$%.1f",tip)
+        totalLabel.text = String(format:"$%.2f",total)
+    }
+    
+    }
+    
 
